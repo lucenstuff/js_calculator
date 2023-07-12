4e545ac241374269632efece5f05e4012b245b70
Lucen <lucentiniagustin@hotmail.com>
2023-07-12 20:55:18 -0300

    first commit

---
+++ calculator-logic.js
@@ -0,0 +1,39 @@
+
+const display = document.querySelector(".display");
+const button = document.querySelectorAll(".btn");
+
+button.forEach(button => {
+    button.addEventListener("click", () => {
+        const clickedButton = button.textContent;
+
+        if (button.id === "c") {
+            display.textContent = "0";
+            return;
+        }
+
+        if (button.id === "del") {
+            if (display.textContent.length === 1 || display.textContent === "Error!") {
+                display.textContent = "0";
+            } else {
+                display.textContent = display.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if (button.id === "equals") {
+            try {
+                display.textContent = eval(display.textContent);
+            } catch {
+                display.textContent = "Error!";
+            }
+            return;
+        }
+
+        if (display.textContent === "0" || display.textContent === "Error!") {
+            display.textContent = clickedButton;
+        } else {
+            display.textContent += clickedButton;
+        }
+        
+    })
+})
